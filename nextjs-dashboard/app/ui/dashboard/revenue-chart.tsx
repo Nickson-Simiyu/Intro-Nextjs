c13329c17078f112d36336ef691cde0bbfa6cf72
Nickson-Simiyu <simiyunickson1@gmail.com>
2024-09-06 17:58:23 +0300

    "Mutating data handling errors adding authentication" This reverts commit ea1540b5aaecb51d8570251f839a25cf53481439.

--- nextjs-dashboard/app/ui/dashboard/revenue-chart.tsx
+++ nextjs-dashboard/app/ui/dashboard/revenue-chart.tsx
@@ -1,7 +1,7 @@
 import { generateYAxis } from '@/app/lib/utils';
 import { CalendarIcon } from '@heroicons/react/24/outline';
 import { lusitana } from '@/app/ui/fonts';
-import { fetchRevenue } from '@/app/lib/definitions';
+import { Revenue } from '@/app/lib/definitions';
 
 // This component is representational only.
 // For data visualization UI, check out:
@@ -9,11 +9,14 @@ import { fetchRevenue } from '@/app/lib/definitions';
 // https://www.chartjs.org/
 // https://airbnb.io/visx/
 
-export default async function RevenueChart(){
-  const revenue = await fetchRevenue();
-  
+export default async function RevenueChart({
+  revenue,
+}: {
+  revenue: Revenue[];
+}) {
   const chartHeight = 350;
   // NOTE: Uncomment this code in Chapter 7
+
   const { yAxisLabels, topLabel } = generateYAxis(revenue);
 
   if (!revenue || revenue.length === 0) {
