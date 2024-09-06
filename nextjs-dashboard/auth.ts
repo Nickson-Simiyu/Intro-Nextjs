f55cd8896937a6cddf06331b368556135d15d882
Nickson-Simiyu <simiyunickson1@gmail.com>
2024-09-06 21:55:36 +0300

    auth ts

--- nextjs-dashboard/auth.ts
+++ nextjs-dashboard/auth.ts
@@ -27,10 +27,12 @@ export const { auth, signIn, signOut } = NextAuth({
                     .safeParse(credentials);
 
                     if (parsedCredentials.success) {
-                        const { email, password } = parsedCredentials.data;
-                        const user = await getUser(email);
-                        if (!user) return null;
-                        const passwordsMatch = await bcrypt.compare(password, user.password);
+                      const { email, password } = parsedCredentials.data;
+                      const user = await getUser(email);
+                      if (!user) return null;
+                      const passwordsMatch = await bcrypt.compare(password, user.password);
+
+                      if (passwordsMatch) return user;
                     }
 
                     console.log('Invalid credentials');
